dac990e46b943e1e303b9867ecdbb2de381d892f
SevanBrodjian <sevanbro7@gmail.com>
2022-07-18 08:35:39 -0500

    Added new DNS to allowed hosts

--- sevanbnet/settings.py
+++ sevanbnet/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["sevanb.herokuapp.com", "sevanb.net", "127.0.0.1"]
+ALLOWED_HOSTS = ["sevanb.herokuapp.com", "sevanb.net", "127.0.0.1", "www.sevanb.net"]
 
 
 # Application definition
