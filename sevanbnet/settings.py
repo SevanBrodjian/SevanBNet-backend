c38736a8cc33b99203288deac4d95695d31bbf73
SevanBrodjian <sevanbro7@gmail.com>
2022-07-22 15:57:21 -0500

    Enabled debug for heroku troubleshooting

--- sevanbnet/settings.py
+++ sevanbnet/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True #os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ["sevanb.herokuapp.com", "sevanb.net", "127.0.0.1", "www.sevanb.net"]
 
