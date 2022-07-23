03d698b85c832a599cfdba2919bdc564ba4623c7
SevanBrodjian <sevanbro7@gmail.com>
2022-07-23 13:42:39 -0500

    Added logging info

--- sevanbnet/settings.py
+++ sevanbnet/settings.py
@@ -147,3 +147,19 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
